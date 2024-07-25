5c5e9e758faf8a7eaedf502266b21ba16f686b58
saremisadra <sadra@nematifamilyfundation.onmicrosoft.com>
2024-07-25 13:38:30 +0330

    Update safeEval.py

--- indox/IndoxEval/safety_eval/safeEval.py
+++ indox/IndoxEval/safety_eval/safeEval.py
@@ -15,7 +15,22 @@ class Verdicts(BaseModel):
 class Reason(BaseModel):
     reason: str
 
-class SafetyEvaluation:
+import json
+from typing import List
+from pydantic import BaseModel, Field
+from transformers import pipeline
+from textblob import TextBlob
+class SafetyEvaluationVerdict(BaseModel):
+    safe: str
+    reason: str = Field(default=None)
+
+class Verdicts(BaseModel):
+    verdicts: List[SafetyEvaluationVerdict]
+
+class Reason(BaseModel):
+    reason: str
+
+class SafeEval:
     def __init__(self, query: str, retrieval_context: List[str]):
         self.toxicity_model = pipeline("text-classification", model="cardiffnlp/twitter-roberta-base-sentiment")
         self.hate_speech_model = pipeline("text-classification", model="Hate-speech-CNERG/dehatebert-mono-english")
@@ -96,3 +111,5 @@ class SafetyEvaluation:
     def _call_language_model(self, prompt: str) -> str:
         response = self.model.generate_evaluation_response(prompt=prompt)
         return response
+
+
