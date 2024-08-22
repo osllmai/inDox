bf352ad456c848691e6bfa19c2979e74d50dd881
saremisadra <sadra@nematifamilyfundation.onmicrosoft.com>
2024-08-22 14:16:37 +0330

    Add files via upload

---
+++ indox/data_loader/json.py
@@ -0,0 +1,53 @@
+import os
+import json
+from indox.core.document_object import Document
+from typing import List
+
+class Json:
+    """
+    Load a JSON file and return its content as a list of `Document` objects.
+
+    Parameters:
+    - file_path (str): The path to the JSON file to be loaded.
+
+    Methods:
+    - load_file(): Loads the JSON file and returns a list of `Document` objects, each containing a JSON
+                   key-value pair and associated metadata.
+
+    Raises:
+    - RuntimeError: If there is an error in loading the JSON file.
+
+    Notes:
+    - Metadata includes the file name and the number of entries in the JSON data.
+    - Each JSON key-value pair is stored as a string in a separate `Document` object.
+    """
+
+    def __init__(self, file_path: str):
+        self.file_path = os.path.abspath(file_path)
+
+    def load(self) -> List[Document]:
+        try:
+            with open(self.file_path, 'r', encoding='utf-8') as f:
+                json_data = json.load(f)
+                documents = []
+
+                # Metadata for all entries
+                metadata_dict = {
+                    'source': self.file_path,
+                    'num_entries': len(json_data),
+                }
+
+                for key, value in json_data.items():
+                    content = f"{key}: {value}"
+                    document_metadata = {
+                        'source': self.file_path,
+                        'key': key
+                    }
+                    document = Document(page_content=content, metadata=document_metadata)
+                    documents.append(document)
+
+                return documents
+        except Exception as e:
+            raise RuntimeError(f"Error loading JSON file: {e}")
+
+
---
+++ indox/data_loader/pypdf4.py
@@ -0,0 +1,56 @@
+from indox.core.document_object import Document
+from typing import List
+import os
+
+class PyPdf4:
+    """
+    Load a PDF file and extract its text and metadata using PyPDF4.
+
+    Parameters:
+    - file_path (str): The path to the PDF file to be loaded.
+
+    Methods:
+    - load_file(): Loads the PDF file and returns a list containing `Document` objects, each with the
+                   text content of a page and the associated metadata.
+
+    Raises:
+    - FileNotFoundError: If the specified file does not exist.
+    - RuntimeError: If there is an error reading the PDF file or extracting text.
+    """
+
+    def __init__(self, file_path: str):
+        self.file_path = os.path.abspath(file_path)
+
+    def load(self) -> List[Document]:
+        import PyPDF4
+
+        try:
+            with open(self.file_path, 'rb') as f:
+                try:
+                    reader = PyPDF4.PdfFileReader(f)
+                except PyPDF4.utils.PdfReadError as e:
+                    raise RuntimeError(f"Error reading PDF file: {self.file_path}. Details: {e}")
+
+                documents = []
+                for i in range(reader.getNumPages()):
+                    page = reader.getPage(i)
+                    try:
+                        text = page.extractText()
+                    except Exception as e:
+                        raise RuntimeError(f"Error extracting text from page {i}. Details: {e}")
+
+                    metadata_dict = {
+                        'source': self.file_path,
+                        'page': i
+                    }
+
+                    document = Document(page_content=text, metadata=metadata_dict)
+                    documents.append(document)
+
+                return documents
+        except FileNotFoundError as e:
+            raise FileNotFoundError(f"File not found: {self.file_path}. Details: {e}")
+        except Exception as e:
+            raise RuntimeError(f"Unexpected error while processing PDF file: {self.file_path}. Details: {e}")
+
+
