2c3c105423175f94fdf28b2f4e6f11ec4eca4837
saremisadra <sadra@nematifamilyfundation.onmicrosoft.com>
2024-08-25 08:06:08 -0700

    Update __init__.py

--- indox/data_connector/google/__init__.py
+++ indox/data_connector/google/__init__.py
@@ -1,1 +1,5 @@
-
+from .googleDoc import GoogleDoc
+from .goolgeSheet import GoogleSheet
+from .googleDrive import GoogleDrive
+from .googleChat import GoogleChat
+from .maps_search import MapsTextSearch
