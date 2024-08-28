9376c3fe85ca6b0c3dcadd6419593573bc2ae345
unknown <ashkan.eskandari.dev@gmail.com>
2024-08-28 09:04:38 +0330

    versioning

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='Indox',
-    version='0.1.24',
+    version='0.1.25',
     license='AGPL-3.0',
     packages=find_packages(),
     include_package_data=True,
