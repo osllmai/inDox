0861b374bd156a81c47b30b48cc2dcc187157bc8
unknown <ashkan.eskandari.dev@gmail.com>
2024-09-10 17:02:30 +0330

    fix imports

--- indox/vector_stores/__init__.py
+++ indox/vector_stores/__init__.py
@@ -10,10 +10,10 @@ from .milvus import Milvus
 from .pathway import PathwayVectorClient
 from .duckdb import DuckDB
 # from .neo4j_vector import Neo4jVector
-# from .redis import RedisDB
+from .redis import RedisDB
 from .qdrant import Qdrant
-# from .lantern import LanternDB
-# from .singlestore import SingleStoreVectorDB
+from .lantern import LanternDB
+from .singlestore import SingleStoreVectorDB
 from .couchBase import Couchbase
 from .vespa import VESPA
 from .deeplake import Deeplake
