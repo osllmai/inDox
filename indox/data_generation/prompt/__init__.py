73f6496415ffb528db30e94be4ceb49bfd4a0db7
sadra saremi <sadra@nematifamilyfundation.onmicrosoft.com>
2024-09-22 23:14:49 -0700

    Delete indox/data_generation directory

--- indox/data_generation/prompt/__init__.py
+++
@@ -1,3 +0,0 @@
-from .data_from_prompt import DataFromPrompt
-from .few_shot_prompt import FewShotPrompt
-from .prompt_base import PromptBase
--- indox/data_generation/upload_data/__init__.py
+++
@@ -1,4 +0,0 @@
-from .document_object import Document
-from .excel import Excel
-from .csv import CSV
-from .load import load_data
