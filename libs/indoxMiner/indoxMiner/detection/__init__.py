7b068711e9b2ab3c430f0840811f30ed6903619d
fatemegh98 <fatemeghasemi491@gmail.com>
2025-01-04 19:39:47 +0330

    GroundedSAM2, GroundedSAM2Florence2 added

--- libs/indoxMiner/indoxMiner/detection/__init__.py
+++ libs/indoxMiner/indoxMiner/detection/__init__.py
@@ -8,6 +8,7 @@ from .models.owlvit.owlvit import OWLVit
 from .models.rtdetr.rtdetr import RTDETR
 from .models.sam2.sam2 import SAM2
 from .models.groundedsam2.groundedsam2 import GroundedSAM2
+from .models.groundedsam2.groundedsam2florence2 import GroundedSAM2Florence2
 from .models.YOLO.YOLOv5.yolov5 import YOLOv5
 from .models.YOLO.YOLOv6.yolov6 import YOLOv6
 from .models.YOLO.YOLOv7.yolov7 import YOLOv7
