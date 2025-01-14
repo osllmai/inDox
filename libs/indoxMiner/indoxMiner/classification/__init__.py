15f544b6789e9a1d043d88b7c201d99f0ff4b3d7
fatemegh98 <fatemeghasemi491@gmail.com>
2025-01-14 16:35:55 +0330

    classification models updated.

---
+++ libs/indoxMiner/indoxMiner/classification/__init__.py
@@ -0,0 +1,13 @@
+from .sigclip_classifier import SigCLIPClassifier
+from .vit_classifier import ViTClassifier
+from .metaclip_classifier import MetaCLIPClassifier
+from .mobileclip_classifier import MobileCLIPClassifier
+from .bioclip_classifier import BioCLIPClassifier
+
+__all__ = [
+    "SigCLIPClassifier",
+    "ViTClassifier",
+    "MetaCLIPClassifier",
+    "MobileCLIPClassifier",
+    "BioCLIPClassifier",
+]
